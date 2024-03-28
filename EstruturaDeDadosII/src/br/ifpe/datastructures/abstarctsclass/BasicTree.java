ee1589e6053c95443d65a447e9106b04fd71f37c
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-03-28 20:33:13 -0300

    Implementação da classe BasicNode

--- EstruturaDeDadosII/src/br/ifpe/datastructures/abstarctsclass/BasicTree.java
+++ EstruturaDeDadosII/src/br/ifpe/datastructures/abstarctsclass/BasicTree.java
@@ -1,5 +1,32 @@
 package br.ifpe.datastructures.abstarctsclass;
 
-public abstract class BasicTree {
+import br.ifpe.interfaces.IStructures;
 
+public abstract class BasicTree implements IStructures {
+	
+	private int tamanho;
+	private BasicNode rootNode;
+	
+	public BasicTree() {
+		super();
+		tamanho = 0;
+		rootNode = null;
+		
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public BasicNode getRootNode() {
+		return rootNode;
+	}
+
+	public void setRootNode(BasicNode rootNode) {
+		this.rootNode = rootNode;
+	}
 }
--- EstruturaDeDadosII/src/br/ifpe/interfaces/IStructures.java
+++ EstruturaDeDadosII/src/br/ifpe/interfaces/IStructures.java
@@ -1,5 +1,8 @@
 package br.ifpe.interfaces;
 
 public interface IStructures {
+	
+	String addNode(String value, String occurrence);
+	String getNode(String value);
 
 }
