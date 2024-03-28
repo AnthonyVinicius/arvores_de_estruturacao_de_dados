0647bfc66f4c0003d32c9da594fc9796c6b4cd45
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-03-28 20:20:01 -0300

    Adição da classe abstrata BasicNode

--- EstruturaDeDadosII/src/br/ifpe/datastructures/abstarctclass/BasicNode.java
+++ EstruturaDeDadosII/src/br/ifpe/datastructures/abstarctclass/BasicNode.java
@@ -1,5 +1,55 @@
 package br.ifpe.datastructures.abstarctclass;
 
-public class BasicNode {
+public abstract class BasicNode {
+	
+	private BasicNode leftSon;
+	private BasicNode rightSon;
+	private BasicNode father;
+	private String binaryCode;
+	private Integer value;
+	
+	public BasicNode(Integer value) {
+		super();
+		this.value = value;
+	}
 
+	public BasicNode getLeftSon() {
+		return leftSon;
+	}
+
+	public void setLeftSon(BasicNode leftSon) {
+		this.leftSon = leftSon;
+	}
+
+	public BasicNode getRightSon() {
+		return rightSon;
+	}
+
+	public void setRightSon(BasicNode rightSon) {
+		this.rightSon = rightSon;
+	}
+
+	public BasicNode getFather() {
+		return father;
+	}
+
+	public void setFather(BasicNode father) {
+		this.father = father;
+	}
+
+	public String getBinaryCode() {
+		return binaryCode;
+	}
+
+	public void setBinaryCode(String binaryCode) {
+		this.binaryCode = binaryCode;
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
 }
