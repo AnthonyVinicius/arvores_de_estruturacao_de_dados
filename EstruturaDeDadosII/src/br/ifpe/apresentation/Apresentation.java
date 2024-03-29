543efaa8bce56035f7a2744928a2c1aa2d2cc863
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-03-28 21:10:33 -0300

    Classe apresentação com erros

--- EstruturaDeDadosII/src/br/ifpe/apresentation/Apresentation.java
+++ EstruturaDeDadosII/src/br/ifpe/apresentation/Apresentation.java
@@ -1,5 +1,88 @@
 package br.ifpe.apresentation;
 
-public class Apresentation {
+import java.util.Scanner;
 
+import br.ifpe.datastructures.trees.BinaryTree;
+import br.ifpe.datastructures.trees.HuffamanTree;
+
+public class Apresentation {
+	
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void ui() {
+
+		while (true) {
+
+			System.out.println("[1] - Arvore Binaria / [2] - Arvore Huffman");
+			String input = scanner.nextLine();
+
+			switch (input) {
+			case "1": {
+				manipularBinaryTree();
+			}
+
+			case "2": {
+				manipularHuffmanTree();
+			}
+
+			default:
+				System.out.println("A entrada: " + input + "é invalida");
+
+			}
+		}
+	}
+
+	private static void manipularBinaryTree() {
+	    BinaryTree binaryTree = new BinaryTree();
+	    
+	    System.out.println("[1] - add(int Value): \n[2] - get(int Value):");
+	    String input = scanner.nextLine();
+
+	    switch (input) {
+	        case "1": {
+	            System.out.println("Elemento :");
+	            String value = scanner.nextLine();
+	            binaryTree.addNode(value, null);
+	            break; // Adicionado o break aqui
+	        }
+	        case "2": {
+	            String value = scanner.nextLine();
+	            binaryTree.getNode(value);
+	            break; // Adicionado o break aqui
+	        }
+	        default:
+	            System.out.println("A entrada: " + input + "é invalida");
+	            break; // Adicionado o break aqui
+	    }
+
+	    manipularBinaryTree(); // Loop
+	}
+
+	private static void manipularHuffmanTree() {
+	    HuffamanTree huffamanTree = new HuffamanTree();
+	    
+	    System.out.println("[1] - add(String): \n[2] - get(Value: )");
+	    String input = scanner.nextLine();
+
+	    switch (input) {
+	        case "1": {
+	            System.out.println("Elemento :");
+	            String value = scanner.nextLine();
+	            System.out.println("Ocorrencia :");
+	            String occurrence = scanner.nextLine();
+	            huffamanTree.addNode(value, occurrence); 
+	            break; // Adicionado o break aqui
+	        }
+	        case "2": {
+	            String value = scanner.nextLine();
+	            huffamanTree.getNode(value);
+	            break; // Adicionado o break aqui
+	        }
+	        default:
+	            System.out.println("A entrada: " + input + " é invalida");
+	            break; // Adicionado o break aqui
+	    }
+
+	    manipularHuffmanTree(); // Loop
+	}
 }
--- EstruturaDeDadosII/src/br/ifpe/datastructures/trees/BinaryTree.java
+++ EstruturaDeDadosII/src/br/ifpe/datastructures/trees/BinaryTree.java
@@ -1,5 +1,21 @@
 package br.ifpe.datastructures.trees;
 
-public class BinaryTree {
+import br.ifpe.datastructures.abstarctsclass.BasicTree;
+
+public class BinaryTree extends BasicTree {
+	
+	private BinaryTree rootNode;
+
+	@Override
+	public String addNode(String value, String occurrence) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNode(String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
