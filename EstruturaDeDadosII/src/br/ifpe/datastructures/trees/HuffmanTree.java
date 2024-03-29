1c4bcb3c8e15982b6b7f6a8c5015dcbeaf77cd76
LuizBuarqueDev <luizgabrielbuarque@gmail.com>
2024-03-28 21:17:24 -0300

    Implemetação arvore de huffman

--- EstruturaDeDadosII/src/br/ifpe/datastructures/trees/HuffmanTree.java
+++ EstruturaDeDadosII/src/br/ifpe/datastructures/trees/HuffmanTree.java
@@ -1,5 +1,18 @@
 package br.ifpe.datastructures.trees;
 
-public class HuffmanTree {
+import br.ifpe.datastructures.abstarctsclass.BasicTree;
 
+public class HuffmanTree extends BasicTree {
+
+	@Override
+	public String addNode(String value, String occurrence) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNode(String value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
