a28de5bc224651c689bdd074b20b9c7efb3d1622
Anthony Vinicius <anthonyvinicius1312@gmail.com>
2024-03-28 22:54:46 -0300

    Huffman

--- EstruturaDeDadosII/src/br/ifpe/datastructures/node/HuffmanNode.java
+++ EstruturaDeDadosII/src/br/ifpe/datastructures/node/HuffmanNode.java
@@ -3,9 +3,27 @@ package br.ifpe.datastructures.node;
 import br.ifpe.datastructures.abstarctsclass.BasicNode;
 
 public class HuffmanNode extends BasicNode {
-
+	
+	private String value;
+	
+	
 	public HuffmanNode() {
-		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	public HuffmanNode(String value) {
+		super();
+		this.value = value;
+		
+	}
+
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+
 }
--- EstruturaDeDadosII/src/br/ifpe/datastructures/trees/HuffmanTree.java
+++ EstruturaDeDadosII/src/br/ifpe/datastructures/trees/HuffmanTree.java
@@ -1,8 +1,13 @@
 package br.ifpe.datastructures.trees;
 
 import br.ifpe.datastructures.abstarctsclass.BasicTree;
+import br.ifpe.datastructures.node.HuffmanNode;
 
 public class HuffmanTree extends BasicTree {
+
+	private String rootNode;
+
+	HuffmanNode node = new HuffmanNode();
 	
 	public HuffmanTree() {
 		// TODO Auto-generated constructor stub
@@ -10,10 +15,26 @@ public class HuffmanTree extends BasicTree {
 
 	@Override
 	public String addNode(String value, String occurrence) {
-		// TODO Auto-generated method stub
+		return null;
+		}
+	
+	
+	public String addNode(String value, int occurence) {
+		
+		if(rootNode == null) {
+		
+			rootNode = value;
+			
+		}else {
+			node.setValue(value);
+			node.setFather(node);
+			
+		}
+		
 		return null;
 	}
-
+	
+	
 	@Override
 	public String getNode(String value) {
 		// TODO Auto-generated method stub
